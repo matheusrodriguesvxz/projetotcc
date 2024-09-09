72906248567b7300b8bfd9f833778b3e106433e4
gabriel fontes <gabriel.sousa235@etec.sp.gov.br>
2024-09-09 17:59:05 -0300

    Desenvolvendo tela de login

--- ReactNative/src/app/login.tsx
+++ ReactNative/src/app/login.tsx
@@ -1,32 +1,50 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Logo, RoxoLogin } from "../components/Svgs";
-import { View, Text, TextInput } from "react-native";
+import { LoginGoogle, Logo, RoxoLogin } from "../components/Svgs";
+import { View, Text, TextInput, Pressable } from "react-native";
 import { useFonts } from "expo-font";
+import { ButaoRegistro } from "../components/index/indexComp";
+import { ButaoLogar } from "../components/Login";
+import { router } from "expo-router";
 
 export default function LoginPage() {
     const [loaded] = useFonts({
         Poppins: require("../../assets/fonts/Poppins-Bold.ttf"),
     });
+
     return (
-        <View className="bg-white">
-            <View className="bg-black">
+        <><View className="bg-white">
+            <View className="bg-black ">
                 <RoxoLogin />
             </View>
             <View className="h-[50%] w-full justify-center items-center mt-3">
                 <Logo />
             </View>
-            <View className="flex relative w-[100] mt-9 ml-6">
-                <Text className="" style={{ fontSize: 25, fontFamily: "Poppins", fontWeight: "bold", color: "#000000", letterSpacing: 0.5, marginTop: 5 }}
+            <View className="flex relative w-[300] mt-9 ml-6">
+                <Text className="" style={{ fontSize: 25, fontFamily: "Poppins", fontWeight: "bold", color: "#000000", letterSpacing: 0.5, marginTop: -30 }}
                 >Vamos Começar!</Text>
-                <Text style={{ fontSize: 15, fontFamily: "Poppins", fontWeight: "bold", color: "#B0B0B0", letterSpacing: 0.5, marginTop: 5 }}> Entre em sua conta para continuar</Text>
+                <Text style={{ fontSize: 15, fontFamily: "Poppins", fontWeight: "bold", color: "#B0B0B0", letterSpacing: 0.5, marginTop: 10 }}> Entre em sua conta para continuar</Text>
             </View>
 
             <TextInput className="mt-8 border-1 border-b-4  py-2 w-[315]  ml-6 border-purple-800"
                 placeholder="Usuario" placeholderTextColor="#909090"
-                style={{ fontFamily: "Poppins", fontWeight: "bold", color: "#760BFF", letterSpacing: 0.5, marginTop: 25, fontSize: 16, borderColor: "#760BFF" }}
-            />
+                style={{ fontFamily: "Poppins", fontWeight: "bold", color: "#760BFF", letterSpacing: 0.5, marginTop: 25, fontSize: 16, borderColor: "#760BFF" }} />
+            <TextInput className="mt-20 border-1 border-b-4 py-2 w-[315] ml-6 border-purple-800   "
+                placeholder="Senha" secureTextEntry placeholderTextColor="#909090"
+                style={{ fontFamily: "Poppins", fontWeight: "bold", color: "#000000", letterSpacing: 0.5, marginTop: 55, fontSize: 16, borderColor: "#760bFF" }} />
+
 
         </View>
+        <View className="h-[50%] w-full items-center mt-[-60]">
+            <Text className="mb-4"   style={{ fontFamily: "Poppins", color: "#B0B0B0", fontWeight: "bold"}}>Ou entre com:</Text>
+            <ButaoLogar/>
+
+            <Pressable onPress={() => {router.push("/homePage")} }>
+                <Text className=""  style={{ fontFamily: "Poppins", color: "#B0B0B0", fontWeight: "bold"}}>Esqueceu sua Senha? Clique aqui para Redefinir</Text>
+            </Pressable>
+
+            <LoginGoogle/>
+        </View>
+        </>
 
     );
 }
--- ReactNative/src/components/Login/index.tsx
+++ ReactNative/src/components/Login/index.tsx
@@ -1,5 +1,8 @@
-import { View } from "react-native";
-
+import { useFonts } from "expo-font";
+import * as Svg from "react-native-svg";
+import { View, Image, StyleSheet, Pressable, Text, TouchableOpacity } from "react-native";
+import { router } from "expo-router";
+import { BackgroundPurple } from "../Svgs";
 export function LoginPage(){
     return(
         <View className="">
@@ -13,3 +16,34 @@ export function LoginPage(){
 }
 
 
+export function ButaoLogar() {
+    return (
+      <TouchableOpacity  onPress={() => {router.push("/homePage")}} style={style.butaoLogar}>
+        <Text style={style.TextobutaoLogar}>Entrar</Text>
+      </TouchableOpacity>
+    );
+  }
+  
+  const style = StyleSheet.create({
+    butaoLogar: {
+      justifyContent: "center",
+      position: "relative",
+      top: 0,
+      left: 0,
+      width: 218,
+      paddingVertical: 12,
+      backgroundColor: "#000000",
+      paddingHorizontal: 3,
+      borderRadius: 20,
+      marginBottom: 30,
+    },
+    TextobutaoLogar: {
+      fontFamily: "Poppins",
+      color: "white",
+      textAlign: "center",
+      fontSize: 20,
+      fontWeight: "bold",
+      letterSpacing: 1,
+    },
+  });
+  
