f487f5b0b54f047723a813d7ba5ad174893022e5
Joao Vitor Diamon Roldan dos Santos <143654414+JoaoVitorDiamon@users.noreply.github.com>
2024-09-16 23:17:46 -0300

    Arrumando o RegisterPage

--- ReactNative/src/app/registerPage.tsx
+++ ReactNative/src/app/registerPage.tsx
@@ -1,297 +1,62 @@
-import React from "react";
-import {
-  View,
-  Image,
-  Text,
-  TextInput,
-  Pressable,
-} from "react-native";
-import { useFonts } from "expo-font";
-import { CheckBox } from "@rneui/themed";
-import { Controller, useForm,type SubmitHandler } from "react-hook-form";
+import { ImageBackground, View, Image, Text, TextInput } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Logo } from "../components/Svgs";
+import { useFonts } from "expo-font";
+import { CheckBox } from '@rneui/themed';
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
 import { ButaoRegistro } from "../components/RegisterPage/RegisterPageComp";
 
-type FormData = {
-  name: string;
-  email: string;
-  password: string;
-  passwordConfirmation: string;
-  terms: boolean;
-};
-
 export default function RegisterPage() {
-  const [loaded] = useFonts({
-    PoppinsBold: require("../../assets/fonts/Poppins-Bold.ttf"),
-  });
+    const [loaded, arial] = useFonts({
+        PoppinsBold: require("../../assets/fonts/Poppins-Bold.ttf"),
+    });
 
-  const {
-    control,
-    handleSubmit,
-    setError,
-    clearErrors,
-    formState: { errors, isSubmitted },
-    getValues,
-    register,
-  } = useForm<FormData>();
+    const [checked, setChecked] = useState(false);
 
-  // Função chamada ao submeter o formulário
-  const onSubmit: SubmitHandler<FormData> = (data) => {
-    // Validação da confirmação da senha
-    if (data.password !== data.passwordConfirmation) {
-      setError("passwordConfirmation", {
-        type: "manual",
-        message: "As senhas não coincidem.",
-      });
-      return;
-    }
+    return (
 
-    // Validação dos termos de serviço
-    if (!data.terms) {
-      setError("terms", {
-        type: "manual",
-        message: "Você deve aceitar os termos.",
-      });
-      return;
-    }
+        <View className="h-full bg-white">
+            <Image source={require("../../assets/purplebg.png")} className="h-100 w-100 z-index -1 right-[7] bottom-1" />
+            <View className=" w-full h-52 absolute items-center mt-28">
+                <Logo />
+            </View>
+            <View className="bg-white rounded-[80] h-[80] top-[-50] w-full p-9" >
 
-    // Se não houver erros, exibe os dados no console
-    console.log("Formulário enviado:", data);
-  };
+                <View className="absolute left-11 top-14">
+                    <Text
+                        style={{ fontFamily: "Poppins", fontWeight: "bold", color: "#760FFF", letterSpacing: 0.5, marginTop: 5, fontSize: 23 }}>
+                        Criar Conta
+                    </Text>
 
-  return (
-    <View className="h-full bg-white">
-      <Image
-        source={require("../../assets/purplebg.png")}
-        className="h-100 w-100 z-index -1 right-[7] bottom-1"
-      />
-      <View className="w-full h-52 absolute items-center mt-28">
-        <Logo />
-      </View>
-      <View className="bg-white rounded-[80] h-[80] top-[-50] w-full p-9">
-        <View className="absolute left-11 top-14">
-          <Text
-            style={{
-              fontFamily: "Poppins",
-              fontWeight: "bold",
-              color: "#760FFF",
-              letterSpacing: 0.5,
-              marginTop: 5,
-              fontSize: 23,
-            }}
-          >
-            Criar Conta
-          </Text>
+                    <TextInput className="mt-8 border-1 border-b-4 py-2 w-[315] border-purple-800   "
+                        placeholder="Nome completo" placeholderTextColor="#909090"
+                        style={{ fontFamily: "Poppins", fontWeight: "bold", color: "#000000", letterSpacing: 0.5, marginTop: 25, fontSize: 16, borderColor: "#760bFF" }} />
 
-          {/* Campo Nome */}
-          <Controller
-            control={control}
-            name="name"
-            rules={{
-              required: "Nome obrigatório.",
-              pattern: {
-                value: /^[A-Za-z]+(?: [A-Za-z]+)*$/,
-                message: "Nome inválido",
-              },
-            }}
-            render={({ field: { onChange, value } }) => (
-              <View>
-                <TextInput
-                  className="mt-8 border-1 border-b-4 py-2 w-[315] border-purple-800"
-                  onChangeText={onChange}
-                  value={value}
-                  placeholder="Nome completo"
-                  placeholderTextColor="#909090"
-                  style={{
-                    fontFamily: "Poppins",
-                    fontWeight: "bold",
-                    color: "#000000",
-                    letterSpacing: 0.5,
-                    marginTop: 25,
-                    fontSize: 16,
-                    borderColor: "#760bFF",
-                  }}
-                />
-                {/* Exibir erro apenas após o envio */}
-                {isSubmitted && errors.name && (
-                  <Text style={{
-                    color: "red",
-                    marginTop: 3,
-                    fontFamily: "Poppins",
-                    fontWeight: "bold",
-                    letterSpacing: 0.7,
-                    fontSize: 14,
-                  }}>
-                    {errors.name.message}
-                  </Text>
-                )}
-              </View>
-            )}
-          />
+                    <TextInput className="mt-8 border-1 border-b-4 py-2 w-[315] border-purple-800   "
+                        placeholder="E-mail" placeholderTextColor="#909090"
+                        style={{ fontFamily: "Poppins", fontWeight: "bold", color: "#000000", letterSpacing: 0.5, marginTop: 25, fontSize: 16, borderColor: "#760bFF" }} />
 
-          {/* Campo E-mail */}
-          <Controller
-            control={control}
-            name="email"
-            rules={{
-              required: "Email obrigatório.",
-              pattern: {
-                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                message: "Email inválido",
-              },
-            }}
-            render={({ field: { onChange, value } }) => (
-              <View>
-                <TextInput
-                  className="mt-8 border-1 border-b-4 py-2 w-[315] border-purple-800"
-                  onChangeText={onChange}
-                  value={value}
-                  placeholder="E-mail"
-                  placeholderTextColor="#909090"
-                  style={{
-                    fontFamily: "Poppins",
-                    fontWeight: "bold",
-                    color: "#000000",
-                    letterSpacing: 0.5,
-                    marginTop: 25,
-                    fontSize: 16,
-                    borderColor: "#760bFF",
-                  }}
-                />
-                {/* Exibir erro apenas após o envio */}
-                {isSubmitted && errors.email && (
-                  <Text style={{
-                    color: "red",
-                    marginTop: 3,
-                    fontFamily: "Poppins",
-                    fontWeight: "bold",
-                    letterSpacing: 0.7,
-                    fontSize: 14,
-                  }}>
-                    {errors.email.message}
-                  </Text>
-                )}
-              </View>
-            )}
-          />
+                    <TextInput className="mt-8 border-1 border-b-4 py-2 w-[315] border-purple-800   "
+                        placeholder="Senha" secureTextEntry placeholderTextColor="#909090"
+                        style={{ fontFamily: "Poppins", fontWeight: "bold", color: "#000000", letterSpacing: 0.5, marginTop: 25, fontSize: 16, borderColor: "#760bFF" }} />
 
-          {/* Campo Senha */}
-          <Controller
-            control={control}
-            name="password"
-            rules={{
-              required: "Senha obrigatória.",
-              minLength: {
-                value: 8,
-                message: "A senha deve ter pelo menos 8 caracteres.",
-              },
-            }}
-            render={({ field: { onChange, value } }) => (
-              <View>
-                <TextInput
-                  className="mt-8 border-1 border-b-4 py-2 w-[315] border-purple-800"
-                  onChangeText={onChange}
-                  value={value}
-                  secureTextEntry
-                  placeholder="Senha"
-                  placeholderTextColor="#909090"
-                  style={{
-                    fontFamily: "Poppins",
-                    fontWeight: "bold",
-                    color: "#000000",
-                    letterSpacing: 0.5,
-                    marginTop: 25,
-                    fontSize: 16,
-                    borderColor: "#760bFF",
-                  }}
-                />
-                {/* Exibir erro apenas após o envio */}
-                {isSubmitted && errors.password && (
-                  <Text style={{
-                    color: "red",
-                    marginTop: 3,
-                    fontFamily: "Poppins",
-                    fontWeight: "bold",
-                    letterSpacing: 0.7,
-                    fontSize: 14,
-                  }}>
-                    {errors.password.message}
-                  </Text>
-                )}
-              </View>
-            )}
-          />
+                    <TextInput className="mt-8 border-1 border-b-4 py-2 w-[315] border-purple-800   "
+                        placeholder="Confirme sua senha" placeholderTextColor="#909090"
+                        style={{ fontFamily: "Poppins", fontWeight: "bold", color: "#000000", letterSpacing: 0.5, marginTop: 25, fontSize: 16, borderColor: "#760bFF" }} />
 
-          {/* Confirmação de Senha */}
-          <Controller
-            control={control}
-            name="passwordConfirmation"
-            rules={{
-              required: "Confirme sua senha.",
-            }}
-            render={({ field: { onChange, value } }) => (
-              <View>
-                <TextInput
-                  className="mt-8 border-1 border-b-4 py-2 w-[315] border-purple-800"
-                  onChangeText={onChange}
-                  value={value}
-                  secureTextEntry
-                  placeholder="Confirme sua senha"
-                  placeholderTextColor="#909090"
-                  style={{
-                    fontFamily: "Poppins",
-                    fontWeight: "bold",
-                    color: "#000000",
-                    letterSpacing: 0.5,
-                    marginTop: 25,
-                    fontSize: 16,
-                    borderColor: "#760bFF",
-                  }}
-                />
-                {/* Exibir erro apenas após o envio */}
-                {isSubmitted && errors.passwordConfirmation && (
-                  <Text style={{
-                    color: "red",
-                    marginTop: 3,
-                    fontFamily: "Poppins",
-                    fontWeight: "bold",
-                    letterSpacing: 0.7,
-                    fontSize: 14,
-                  }}>
-                    {errors.passwordConfirmation.message}
-                  </Text>
-                )}
-              </View>
-            )}
-          />
 
-          {/* Checkbox de Termos */}
-          <CheckBox
-            className="w-96 right-5"
-            title="Eu li e Aceito os Termos de Serviços e a Politica de Privacidade"
-            {...register("terms", { required: "Você deve aceitar os termos." })}
-            checked={getValues("terms")}
-            checkedColor="#760FFF"
-          />
-          {/* Exibir erro apenas após o envio */}
-          {isSubmitted && errors.terms && (
-            <Text style={{
-              color: "red",
-              marginTop: 3,
-              fontFamily: "Poppins",
-              fontWeight: "bold",
-              letterSpacing: 0.7,
-              fontSize: 14,
-            }}>
-              {errors.terms.message}
-            </Text>
-          )}
+                    <CheckBox
+                        className="w-96 right-5"
+                        title="Eu li e Aceito os Termos de Serviços e a Politica de Privacidade"
+                        checked={checked}
+                        checkedColor="#760FFF"
+                        onPress={() => setChecked(!checked)}
+                        />
+                </View>
+            </View>
+            <ButaoRegistro/>
+
         </View>
-      </View>
-
-      <Pressable onPress={handleSubmit(onSubmit)}>
-        <ButaoRegistro />
-      </Pressable>
-    </View>
-  );
+    );
 }
