6a8f0d86db64fa9a678a7e2b11a0510e7b6b7227
Joao Vitor <joao.santos2112@etec.sp.gov.br>
2024-10-16 17:00:55 -0300

    Feat tela de buyList

--- ReactNative/src/app/_layout.tsx
+++ ReactNative/src/app/_layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout() {
       <Stack.Screen name="registerPage" options={{headerShown:false}}/>
       <Stack.Screen name="login" options={{headerShown:false}}/>
       <Stack.Screen name="playlistPage" options={{headerShown:false}}/>
+      <Stack.Screen name="buyList" options={{headerShown:false}}/>
     </Stack>
   );
 }
