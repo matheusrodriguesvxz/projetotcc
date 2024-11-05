920ffc23e415ea0296067ae48635f7b3ca55294a
Eduarda <eduarda.santos202@etec.sp.gov.br>
2024-11-05 17:16:55 -0300

    Primeira tela de confirmação adicionada

---
+++ siteconfirmacao/src/components/FooterComp.vue
@@ -0,0 +1,22 @@
+
+<template>
+<footer>
+ 
+</footer>
+</template>
+
+<script>
+
+export default {
+    name: 'FooterPage'
+}
+</script>
+
+<style scoped>
+footer{
+    background-image: url("https://t3.ftcdn.net/jpg/03/58/82/46/360_F_358824602_iAyjwiB8xFIuZZOlzJhXFGvpkuuRDiVl.jpg");
+    /* background-repeat: no-repeat; */
+    padding-top: 20px;
+    padding-bottom: 20px;
+}
+</style>
---
+++ siteconfirmacao/src/components/NavComp.vue
@@ -0,0 +1,28 @@
+<template>  
+<header>
+    <nav>
+       <h2>EventEasy</h2>
+    </nav>
+</header>
+</template>
+<script>
+export default {
+    name: 'NavPage'
+}
+</script>
+
+<style >
+*{
+    margin: 0;
+    padding: 0;
+}
+header{
+    background-image: url("https://t3.ftcdn.net/jpg/03/58/82/46/360_F_358824602_iAyjwiB8xFIuZZOlzJhXFGvpkuuRDiVl.jpg");
+    /* background-repeat: no-repeat; */
+    padding-top: 20px;
+    padding-bottom: 20px;
+}
+h2{
+    color: black;
+}
+</style>
